d9fa2dcd126e7a33ece2976a587cc179a9d4e4e6
Henry Logique <hjlogique@yahoo.com>
2021-01-15 17:11:43 -0500

    upload the app

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-react" /> React Google Books Search
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
+import Book from "../components/Book";
+import Footer from "../components/Footer";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+import "./style.css";
+
+class Saved extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    this.getSavedBooks();
+  }
+
+  getSavedBooks = () => {
+    API.getSavedBooks()
+      .then(res =>
+        this.setState({
+          books: res.data
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleBookDelete = id => {
+    API.deleteBook(id).then(res => this.getSavedBooks());
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <div className="transBox">
+                <h1 className="text-center">
+                <i className="fab fa-react" /><strong> React Google Books Search</strong>
+                </h1>
+                <h2 className="text-center">Search for and Save Books of Interest.</h2>
+              </div>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <Card title="Saved Books" icon="download">
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map(book => (
+                    <Book
+                      key={book._id}
+                      title={book.title}
+                      subtitle={book.subtitle}
+                      link={book.link}
+                      authors={book.authors.join(", ")}
+                      description={book.description}
+                      image={book.image}
+                      Button={() => (
+                        <button id="delete"
+                          onClick={() => this.handleBookDelete(book._id)}
+                          className="btn btn-outline-danger ml-2"
+                        >
+                          Delete
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                  <h2 className="text-center">No Saved Books</h2>
+                )}
+            </Card>
+          </Col>
+        </Row>
+        <Footer />
+      </Container>
+    );
+  }
+}
+
+export default Saved;
