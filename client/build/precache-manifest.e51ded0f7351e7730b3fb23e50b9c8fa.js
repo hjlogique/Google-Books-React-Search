9c09849961416d5affe8f1a9f9cc473633a29a78
Henry Logique <hjlogique@yahoo.com>
2021-01-15 22:24:51 -0500

    deploying

---
+++ client/build/precache-manifest.e51ded0f7351e7730b3fb23e50b9c8fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3f84a0f2f867e882d37",
+    "url": "/static/css/main.11eaff87.chunk.css"
+  },
+  {
+    "revision": "b3f84a0f2f867e882d37",
+    "url": "/static/js/main.f4606222.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a2032e8aacd39a53690",
+    "url": "/static/js/2.03e7b06c.chunk.js"
+  },
+  {
+    "revision": "a5c14d4dada752a3e18f979aadcbfe56",
+    "url": "/static/media/google_books.a5c14d4d.jpg"
+  },
+  {
+    "revision": "15ccf2ecef40c0ed98b06d45c3e9a5fe",
+    "url": "/static/media/book_logo.15ccf2ec.png"
+  },
+  {
+    "revision": "f7459e2527b0ed52490b3c67eeda8fae",
+    "url": "/index.html"
+  }
+];
